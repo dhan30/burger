bbd3f2ff702e8869959332b349f404149759c6cc
David Han <hhan0524@gmail.com>
2017-05-18 07:46:22 -0400

    updated

--- config/orm.js
+++ config/orm.js
@@ -1,37 +1,78 @@
-var connection = require("./connection.js");
+var connection = require("../config/connection.js");
 
 // console.log(connection);
 
+var createBurgerlist = function(devoured) {
+	var arr = [];
+	for (var i = 0; i < devoured; i++) {
+		arr.push("_");
+	}
+	return arr.toString();
+}
+
+function objToSql(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    if (Object.hasOwnProperty.call(ob, key)) {
+      arr.push(key + "=" + ob[key]);
+    }
+  }
+
+  return arr.toString();
+}
+
 var orm = {
 
-selectAll: function (a, b, c) {
-	var commandString = "SELECT * FROM burgers WHERE devoured = true";
-
-	connection.query(commandString, [a,b,c], function(err, results) {
-		console.log(results);
-	});
-
-},
-
-insertOne: function (burger_name, devoured, date_devoured) {
-	// console.log("2");
-	var commandString = "UPDATE burgers SET id = 2 WHERE devoured = 0 ";
-	connection.query(commandString, [burger_name,devoured,date_devoured], function(err, results) {
-		console.log(results);
-	});
+selectAll: function(tableInput, cb) {
+    var commandString = "SELECT * FROM " + tableInput + ";";
+    connection.query(commandString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
 
 },
 
-updateOne: function() {
-	console.log("3");
+insertOne:  function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += createBurgerlist(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+
 },
 
-};
+updateOne: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
 
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
 
-// orm.selectAll();
-orm.insertOne("veggieburger", true, 20170525);
-// orm.updateOne();
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
 
+      cb(result);
+    });
+  }
+}
 
 module.exports = orm;
--- server.js
+++ server.js
@@ -1,6 +1,35 @@
 
 // REQUIRE NPM PACKAGES
 var express = require('express');
-var bodyPars = require('body-parser');
-var methodOverRide = require('method-override');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
+// create var to hold port route
+var port = 3001;
+
+// create var to use express method
+var app = express();
+
+// express "use" methods =========================================================================
+app.use(express.static(process.cwd() + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(methodOverride("_method"));
+// ===============================================================================================
+
+// set handlebar routes ==========================================================================
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// import routes and make them accesible
+var routes = require("./controllers/burgers_controller.js");
+
+// home route
+app.use("/", routes);
+
+// making sure port is connected and letting the user know which port to listen on===============
+app.listen(port, function () {
+	console.log ("PORT CHILLING ON: " + port);
+});
